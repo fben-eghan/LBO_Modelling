d84324b68aeaec50d7efa6bf7e1a3a1ac47d701a
Felix Ben-Eghan <122789246+fben-eghan@users.noreply.github.com>
2023-03-10 17:56:28 +0000

    Update PrepRealEstate.sql

--- PrepRealEstate.sql
+++ PrepRealEstate.sql
@@ -1,18 +1,21 @@
-SELECT
-  property_purchase_price,
-  equity_ratio,
-  interest_rate,
-  loan_term,
-  amortization_period,
-  annual_growth_rate,
-  annual_rent_growth_rate,
-  exit_cap_rate,
-  investment_horizon,
-  equity_irr_threshold,
-  annual_rent_income,
-  annual_expenses,
-  tax_rate
-FROM
-  real_estate_properties
-WHERE
-  property_type = 'residential'
+-- This query selects data from the Property table and joins it with the Rent table and Expense table
+-- to calculate annual_rent_income and annual_expenses fields respectively
+SELECT 
+    p.PropertyID, 
+    p.PurchasePrice, 
+    p.YearBuilt, 
+    p.SquareFootage, 
+    r.AnnualRent AS annual_rent_income, 
+    SUM(e.Amount) AS annual_expenses
+FROM 
+    Property p
+    LEFT JOIN Rent r ON p.PropertyID = r.PropertyID
+    LEFT JOIN Expense e ON p.PropertyID = e.PropertyID
+GROUP BY 
+    p.PropertyID, 
+    p.PurchasePrice, 
+    p.YearBuilt, 
+    p.SquareFootage, 
+    r.AnnualRent
+
+-- The above query can be used to populate the annual_rent_income and annual_expenses fields in RealEstateInvestment.py
