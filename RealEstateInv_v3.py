6f130fe6b2f5fe5d54b351342541837f2facec7e
Felix Ben-Eghan <122789246+fben-eghan@users.noreply.github.com>
2023-03-14 10:29:01 +0000

    Update RealEstateInv_v3.py

--- RealEstateInv_v3.py
+++ RealEstateInv_v3.py
@@ -9,6 +9,7 @@ class RealEstateInvestment:
     
     def __init__(self, p=1e6, e=0.25, r=0.06, lt=10, ap=25, agr=0.02, arg=0.02,
                  ecr=0.08, ih=5, irr=0.15, ri=5e4, ex=2.5e4, t=0.2):
+        
         """
         Initialize the RealEstateInvestment class with default values.
         
@@ -27,6 +28,7 @@ class RealEstateInvestment:
            ex (float): Annual property expenses
            t (float): Marginal tax rate
         """
+
         self.p, self.e, self.r, self.lt, self.ap, self.agr = p, e, r, lt, ap, agr
         self.arg, self.ecr, self.ih, self.irr, self.ri, self.ex, self.t = arg, ecr, ih, irr, ri, ex, t
         self.d, self.pmt, self.noi = None, None, None
@@ -34,50 +36,47 @@ class RealEstateInvestment:
         self.p_dist, self.ri_dist, self.ex_dist = None, None, None
         self.agr_dist, self.arg_dist, self.ecr_dist = None, None, None
         self.irr_dist = np.zeros(self.iters)
-
-    def calculate(self, num_sims=10000, **kwargs):
+        
         """
         Simulate cash flows and calculate the probability of achieving the desired IRR.
         """
+
+    def calculate(self, num_sims=10000, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
         res = []
         for _ in range(num_sims):
-            # Calculate down payment and equity.
             self.d, self.e = self.e * self.p, (1 - self.e) * self.p
-            
-            # Calculate monthly payment, net operating income, and annual cash flows.
             self.pmt = npf.pmt(self.r / 12, self.lt * 12, -self.d, 0)
             self.noi = self.ri - self.ex
             annual_cash_flow_before_tax = self.noi - (self.r * self.d + self.pmt) * 12
             annual_cash_flow_after_tax = annual_cash_flow_before_tax * (1 - self.t)
-            
-            # Calculate terminal value and cash flows.
             terminal_value = annual_cash_flow_before_tax * (1 + self.arg) / (self.ecr - self.arg)
             cash_flows = [-self.e] + [annual_cash_flow_after_tax] * self.ih + [terminal_value]
-            
-            # Calculate IRR and append result to res list.
             self.irr_dist[_] = npf.irr(cash_flows)
             res.append(1 if self.irr_dist[_] >= self.irr else 0)
         
-        # Calculate probability of achieving the desired IRR.
         return sum(res) / num_sims
 
-#Create many instances of the RealEstateInvestment class
-
+# Set the number of simulations
 num_sims = 10000
-results = [RealEstateInvestment().calculate(num_sims) for _ in range(num_sims)]
 
+# Create an array of results from running the simulation multiple times
+results = np.array([RealEstateInvestment().calculate(num_sims=num_sims) for _ in range(num_sims)])
+
+# Create a histogram and kernel density estimate of the results
 sns.set_style('whitegrid')
 fig, ax = plt.subplots(figsize=(8, 6))
-
 sns.histplot(results, ax=ax, bins=40, kde=True, stat='density',
-color='tab:blue', alpha=0.8, line_kws={'linewidth': 2})
+             color='tab:blue', alpha=0.8, line_kws={'linewidth': 2})
 sns.kdeplot(results, ax=ax, color='tab:orange', linewidth=3)
 
+# Set the labels and title of the plot
 ax.set_xlabel('IRR Probability', fontsize=14)
 ax.set_ylabel('Density', fontsize=14)
 ax.set_title('Distribution of IRR Probability', fontsize=16)
+
+# Add a legend
 ax.legend(['Kernel Density Estimate', 'Histogram'], fontsize=12)
 
 plt.show()
